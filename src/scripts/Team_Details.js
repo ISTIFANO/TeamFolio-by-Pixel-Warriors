e9aabd9f637c32f5e9c9582d3d76d9e95341e2a8
istifano <elamiriamir7@gmail.com>
2024-11-14 23:53:11 +0100

    modification sur team details

--- src/scripts/Team_Details.js
+++ src/scripts/Team_Details.js
@@ -8,14 +8,14 @@ async function fetchData() {
   
       const data = await response.json();
       console.log(data);
-      
       displayProfile(data.team);
+      displayReviews(data.testimonials);
     } catch (error) {
       console.log("fetch API error", error);
     }
   }
   
-  fetchData();
+  
   
   function displayProfile(team) {
     const profileContainer = document.getElementById("profileContainer");
@@ -51,3 +51,42 @@ async function fetchData() {
     }).join("");
   }
   
+
+  function displayReviews(teamInfo) {
+    const reviews = document.getElementById("reviewsS");
+    reviews.innerHTML = teamInfo
+      .map((testimonial) => {
+        return `<section class="flex items-center justify-center  bg-gray-100">
+
+                    <div class="bg-white shadow-lg rounded-lg p-6 max-w-xs text-center">
+                        <div class="text-orange-500 text-4xl">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="inline-block w-8 h-8" viewBox="0 0 24 24">
+                                <path d="M9 11.4c-.43-2.07.41-4.44 2.31-5.76 1.39-.97 3.05-1.2 4.44-.59 1.39.61 2.48 1.97 2.97 3.47.49 1.5.24 3.06-.68 4.35-1.15 1.64-3.1 2.47-5.04 2.14a4.495 4.495 0 0 1-3.74-3.61c-.06-.36-.14-.71-.26-1.05zM4 11.4c-.43-2.07.41-4.44 2.31-5.76C7.7 4.67 9.36 4.44 10.75 5.05c1.39.61 2.48 1.97 2.97 3.47.49 1.5.24 3.06-.68 4.35C11.89 14.51 9.94 15.34 8 15.01a4.495 4.495 0 0 1-3.74-3.61c-.06-.36-.14-.71-.26-1.05z"/>
+                            </svg>
+                        </div>
+                        <p class="text-gray-600 italic mt-4">
+                            ${testimonial.message}
+                        </p>
+                        <div class="flex justify-center items-center mt-4 space-x-1 text-yellow-500">
+                            <p>${testimonial.Rating}⭐</p>
+                        </div>
+                        <div class="mt-4">
+                            <p class="font-semibold">${testimonial.clientName}</p>
+                            <p class="text-gray-500">${testimonial.date}</p>
+                        </div>
+                    </div>
+                </section>`;
+      })
+      .join("");
+  }
+  
+
+
+
+
+
+
+
+
+
+  fetchData();
