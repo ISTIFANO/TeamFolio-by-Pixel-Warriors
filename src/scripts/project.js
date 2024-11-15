0105938232f0ffd8ad94661a5becc354594ec09e
abdellah <abdo.abdell.2000@gmail.com>
2024-11-15 12:04:00 +0100

    Ajouter l'option de l'ajout*

--- src/scripts/project.js
+++ src/scripts/project.js
@@ -1,15 +1,15 @@
-let tabs= document.querySelectorAll('.tab');
-let indicator = document.querySelector(".indicator")  ;
+ let tabs= document.querySelectorAll('.tab');
+ let indicator = document.querySelector(".indicator")  ;
 
-indicator.style.width = tabs[0].getBoundingClientRect().width + "px";
-indicator.style.left = tabs[0].getBoundingClientRect().left - tabs[0].parentElement.getBoundingClientRect().left + "px";    
+ indicator.style.width = tabs[0].getBoundingClientRect().width + "px";
+ indicator.style.left = tabs[0].getBoundingClientRect().left - tabs[0].parentElement.getBoundingClientRect().left + "px";    
 
-tabs.forEach((tab) => {
+ tabs.forEach((tab) => {
   tab.addEventListener("click", ()=> {
     indicator.style.width = tab.getBoundingClientRect().width + "px";
     indicator.style.left = tab.getBoundingClientRect().left - tabs.parentElement.getBoundingClientRect().left + "px"; 
   })
-})
+ })
       
       var swiper = new Swiper(".mySwiper", {
         slidesPerView: 1,
@@ -41,9 +41,9 @@ tabs.forEach((tab) => {
     function displayProjects(info){
         const allProject = document.getElementById("pro");
         allProject.innerHTML = info.map((item)=>{
-            return `<div class="border-orange-400 shadow-[#5dadec3b] shadow-xl mx-auto p-5 md:p-0 border rounded-2xl md:w-[450px] md:h-96 pd swiper-slide">
-                <img src=${item.image} alt="" class="p-5 border rounded-lg w-full transition-all duration-300 cursor-pointer hover:scale-105">
-                <p class="left-3 bg-orange-300 border rounded-lg text-center">${item.title}</p>
+            return `<div class="border-blue-100 shadow-[#5dadec3b] shadow-xl mx-auto p-5 md:p-0 border rounded-2xl md:w-[450px] md:h-96 pd swiper-slide">
+                <a href=${item.lien}> <img src=${item.image} alt="" class="p-5 border rounded-lg w-full transition-all duration-300 cursor-pointer hover:scale-105"> </a>
+                <p class="left-3 bg-blue-100 border rounded-lg text-center">${item.title}</p>
                </div>`
     
         })
@@ -128,30 +128,24 @@ tabs.forEach((tab) => {
       const reviewCard = document.createElement('section');
       reviewCard.classList.add("flex", "items-center", "justify-center", "h-76", "w-[200px]", "bg-gray-100", "mb-4", "mr-4");
   
-      reviewCard.innerHTML = `
-          <div class="bg-white shadow-lg rounded-lg p-6 text-center">
-              <div class="text-orange-500 text-4xl">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="inline-block w-8 h-8" viewBox="0 0 24 24">
-                      <path d="M9 11.4c-.43-2.07.41-4.44 2.31-5.76 1.39-.97 3.05-1.2 4.44-.59 1.39.61 2.48 1.97 2.97 3.47.49 1.5.24 3.06-.68 4.35-1.15 1.64-3.1 2.47-5.04 2.14a4.495 4.495 0 0 1-3.74-3.61c-.06-.36-.14-.71-.26-1.05zM4 11.4c-.43-2.07.41-4.44 2.31-5.76C7.7 4.67 9.36 4.44 10.75 5.05c1.39.61 2.48 1.97 2.97 3.47.49 1.5.24 3.06-.68 4.35C11.89 14.51 9.94 15.34 8 15.01a4.495 4.495 0 0 1-3.74-3.61c-.06-.36-.14-.71-.26-1.05z"/>
-                  </svg>
-              </div>
-              <p class="text-gray-600 italic mt-4">
-                  ${reviews}
-              </p>
-              <div class="flex justify-center items-center mt-4 space-x-1 text-yellow-500">
-                  <p>${rating}</p>
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" class="w-5 h-5">
-                      <path d="M12 17.3l-4.6 2.7 1.2-5.1-4-3.5 5.2-.4 2-4.9 2 4.9 5.2.4-4 3.5 1.2 5.1-4.6-2.7z"/>
-                  </svg>
-              </div>
-              <div class="mt-4">
-                  <p class="font-semibold">Mr. ${name}</p>
-              </div>
-              ${imageUrl ? `
-                  <div class="mx-auto w-20 h-20 rounded-full overflow-hidden border-4 border-white">
+     reviewCard.innerHTML = `
+          <div class="mt-5 overflow-auto md:overflow-scroll mySwiper swiper-container">
+            <div id="views" class="swiper-wrapper">
+              <div class="px-3 py-8 swiper-slide">
+                ${imageUrl ? `
+                  <div class="mx-auto w-20 h-20 rounded-full overflow-hidden border-4 border-blue-100">
                       <img src="${imageUrl}" alt="Profile Image" class="w-full h-full object-cover">
                   </div>
               ` : ''}
+                <p class="my-5 px-2 text-justify text-sm fa-align-center">
+                ${reviews}
+                </p>
+                <h5 class="mb-2">${name}</h5>
+                <p class="px-2 text-sm">${rating}</p>
+              </div>
+              
+            </div>
+            
           </div>
       `;
       ReviewsSection.appendChild(reviewCard);
@@ -165,5 +159,105 @@ tabs.forEach((tab) => {
   
   
   
-  fetchData();
+
   
+  let toggle = document.getElementById("toggle-modal-btn");
+  toggle.addEventListener("click", function(){
+    var crud = document.getElementById("crud-modal");
+    crud.style = "display:block;";
+  });
+
+
+
+  var cont = 3;
+  var cint = 1;
+
+  let ajouterProject = document.getElementById("ajouter-project");
+  ajouterProject.addEventListener("click", function(){
+      const image_project = document.getElementById("image-project").files[0];
+      let imageUrl = '';
+      if (image_project) {
+          imageUrl = URL.createObjectURL(image_project);
+      }
+      
+      const lien_project = document.getElementById("lien-project").value;
+      const titre = document.getElementById("titre").value;
+      const description = document.getElementById("description").value;
+      
+      const blkdiv = document.getElementById("new-project");
+      blkdiv.insertAdjacentHTML("beforeend", `<div id="${cont - 1} class="flex md:flex-row flex-col justify-between items-center gap-8 my-20 mt-5 md:overflow-scroll overflow-x-scroll mySwiper swiper-container">
+            <div id="pro" class="swiper-wrapper">
+               <div class="border-blue-100 shadow-[#5dadec3b] shadow-xl mx-auto p-5 md:p-0 border rounded-2xl md:w-[450px] md:h-96 pd swiper-slide">
+                <a href="${lien_project}"><img src="${imageUrl}" alt="" class="p-5 border rounded-lg w-full transition-all duration-300 cursor-pointer hover:scale-105"></a>
+                <p class="left-3 bg-blue-100 border rounded-lg text-center">${titre}</p>
+                <p class="left-3 bg-blue-100 border rounded-lg text-center">${description}</p>
+               </div>
+               
+            </div>
+        </div>`
+          )
+          document.getElementById("lien_project").value = " "
+          document.getElementById("titre").value = " "
+          document.getElementById("description").value = " "
+          document.getElementById("image-project").value = '';
+          cont = cont +1;
+
+       
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  let closeBtn = document.getElementById("cancel-btn");
+closeBtn.addEventListener("click", function(){
+    const crud_modal = document.getElementById("crud-modal");
+    crud_modal.style = "display: hidden";
+});
+
+// The function of delete an card service
+function supprifunc(event) {
+    const elementToRemove = event.target.closest('.border-2.border-black.w-72');
+    if (elementToRemove) {
+      elementToRemove.remove();
+    } else {
+      console.error("Element to remove not found");
+    }
+  }
+
+//The function of update
+  function modifierinfo(event) {
+    const project = event.target.closest('.border-2.border-black.w-72');
+    const oldTitle = project.querySelector('h1').textContent;
+    const oldDescription = project.querySelector('small').textContent;
+  
+    project.querySelector('h1').innerHTML = `<input type="text" value="${oldTitle}">`;
+    project.querySelector('small').innerHTML = `<textarea>${oldDescription}</textarea>`;
+  
+    const saveButton = document.createElement('button');
+    saveButton.textContent = 'Save';
+    project.appendChild(saveButton);
+  
+    saveButton.addEventListener('click', () => {
+      project.querySelector('h1').textContent = project.querySelector('input').value;
+      project.querySelector('p').textContent = project.querySelector('input').value;
+      project.querySelector('small').textContent = project.querySelector('textarea').value;
+      saveButton.remove();
+    });
+  }
